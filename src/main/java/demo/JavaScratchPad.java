c53538ec837ed372369bcf93332dcc81dc89cdbd
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-12-22 16:13:43 +0900

    Add jm20211201OperatorPrecedence

---
+++ src/main/java/demo/JavaScratchPad.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class JavaScratchPad {
+
+	public static void main(String[] args) {
+		jm20211201OperatorPrecedence();
+
+	}
+
+	private static void jm20211201OperatorPrecedence() {
+		var res = 54/5*5-4.0+4*8/10;
+		System.out.println(res);
+	}
+	
+}
